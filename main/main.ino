796d680d93fe3ff7adac0728dfe44a3687d36d17
LeoAnderssonKTH <132525577+LeoAnderssonKTH@users.noreply.github.com>
2024-04-24 15:36:15 +0200

    firebase test

--- main/main.ino
+++ main/main.ino
@@ -4,6 +4,18 @@
  * More details: https://github.com/olikraus/u8g2/wiki
  */
 
+ /* 
+ * The FirebaseJsonData object holds the returned data which can be read from the following properties.
+ * jsonData.stringValue - contains the returned string.
+ * jsonData.intValue - contains the returned integer value.
+ * jsonData.floatValue - contains the returned float value.
+ * jsonData.doubleValue - contains the returned double value.
+ * jsonData.boolValue - contains the returned boolean value.
+ * jsonData.success - used to determine the result of the get operation.
+ * jsonData.type - used to determine the type of returned value in string represent 
+ * the types of value e.g. string, int, double, boolean, array, object, null and undefined.
+*/
+
 /****************************************************
  *               Includes               
  *****************************************************/
@@ -11,14 +23,36 @@
 #include "pitches.h"
 #include "ESP8266WiFi.h"
 #include "U8g2lib.h"
+#include "Firebase_ESP_Client.h"
+#include <addons/TokenHelper.h>
 
 /****************************************************
- *               WiFI parameters               
+ *               WiFI & Firebase parameters          
  *****************************************************/
 
-const char* ssid = "Christoffers iPhone 12";
-const char* password = "test123";
+const char* ssid = "iPhone";
+const char* password = "89korvkorv";
 
+#define API_KEY "AIzaSyB1vrEG7FRHz0qx1usl56Nph130NA-KUMM"
+#define PROJECT_ID "meeting-planner-56526"
+#define FIREBASE_AUTH "vc2VyiUJEQZyZrck7bkNDX932JG3"
+
+#define USER_EMAIL "test@test.com"
+#define USER_PASSWORD "test123"
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+FirebaseData dbData;
+
+String uid;
+
+String path;
+
+String firebaseStatus = "on";
 /****************************************************
  *               Pin parameters               
  *****************************************************/
@@ -44,6 +78,8 @@ U8G2_ST7565_NHD_C12864_F_4W_HW_SPI u8g2(U8G2_R2, /* cs=*/ CS, /* dc=*/ RS, /* re
 
 void setup()
 {
+  Serial.begin(115200);
+
   u8g2.begin();  // Initialize the display
   u8g2.setContrast(100); // Adjust the contrast level (0-255)
 
@@ -56,7 +92,38 @@ void setup()
   pinMode(BUZZER, INPUT_PULLUP);
 
   // WiFI
-  //connectToWiFi(ssid, password);
+  connectToWiFi(ssid, password);
+
+  // Set up config API KEY
+  config.api_key = API_KEY;
+
+  // Set up firebase auth
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Generate token
+  config.token_status_callback = tokenStatusCallback;
+
+  // Connect to firebase
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  // Get userID
+  
+  Serial.println("Getting User UID");
+  delay(1000);
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  // Print uid to console
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  delay(1000);
+  Serial.println(uid);
+  delay(1000);
+           
 
   // Buttons
   pinMode(BUTTON_CONFIRM, INPUT_PULLUP);
@@ -69,71 +136,52 @@ void setup()
 
 void loop()
 {
-  byte button_confirm_state = digitalRead(BUTTON_CONFIRM);
-  byte button_abort_state = digitalRead(BUTTON_ABORT);
-  int buttons_direction = analogRead(BUTTONS); // For moving left, right, up and down.
-  
-  u8g2.firstPage();  // Start a page to write graphics
-  do {
-    draw("Room 420");  // Call the draw function where the graphics commands are executed
-  } while ( u8g2.nextPage() );  // Continue to the next page
+  //Get Document
+  //--------------------
+  path = "test/" + uid;
 
-  if (button_confirm_state == LOW || button_abort_state == LOW) {
-      Serial.println("Button is pressed");
-      tone(BUZZER, 540);
+  Serial.print("Get a document... ");
+  delay(1000);
+  if (Firebase.Firestore.getDocument(&fbdo, PROJECT_ID, "", path.c_str(), "")) {
 
-      if(button_confirm_state == LOW){
-          digitalWrite(LED_GREEN, HIGH); 
-          u8g2.firstPage();  // Start a page to write graphics
-          do {
-            draw("Confirm");  // Call the draw function where the graphics commands are executed
-          } while ( u8g2.nextPage() );  // Continue to the next page
-          delay(100);
-      } else{
-          digitalWrite(LED_RED, HIGH); 
-          u8g2.firstPage();  // Start a page to write graphics
-          do {
-            draw("Abort");  // Call the draw function where the graphics commands are executed
-          } while ( u8g2.nextPage() );  // Continue to the next page
-          delay(100);
-      }
-  }
-  else if(button_confirm_state == HIGH && button_abort_state == HIGH && (buttons_direction < 8)){
-      Serial.println("Button is not pressed");
-      noTone(BUZZER);
-      digitalWrite(LED_RED, LOW); 
-      digitalWrite(LED_GREEN, LOW); 
-  }
-  else if(buttons_direction < 225 && buttons_direction > 210){
-    u8g2.firstPage();
-    do{
-      draw("Left");
-    } while(u8g2.nextPage());
-    delay(100);
-  }
-  else if(buttons_direction < 60 && buttons_direction > 50){
-    u8g2.firstPage();
-    do{
-      draw("Right");
-    } while(u8g2.nextPage());
-    delay(100);
-  }
-  else if(buttons_direction < 117 && buttons_direction > 110){
-    u8g2.firstPage();
-    do{
-      draw("Up");
-    } while(u8g2.nextPage());
-    delay(100);
-  }
-  else if(buttons_direction < 910 && buttons_direction > 900){
-    u8g2.firstPage();
-    do{
-      draw("Down");
-    } while(u8g2.nextPage());
-    delay(100);
-  }
-  else{
     
+    Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
+    delay(1000);
+    
+
+    
+
+    // Create a FirebaseJson object and set content with received payload
+    FirebaseJson payload;
+    payload.setJsonData(fbdo.payload().c_str());
+    delay(1000);
+
+
+
+    // Get the data from FirebaseJson object 
+    FirebaseJsonData jsonData;
+    payload.get(jsonData, "fields/test/stringValue", true);
+
+    String test = jsonData.stringValue;
+
+    Serial.println(jsonData.stringValue);
+    delay(1000);
+
+    u8g2.firstPage();
+    do{
+      draw(test.c_str());
+    } while(u8g2.nextPage());
   }
+ 
+  /*u8g2.firstPage();
+    do{
+      draw(FIREBASE_CLIENT_VERSION);
+    } while(u8g2.nextPage());*/
+
+  /*u8g2.firstPage();
+  do{
+    draw(uid.c_str());
+  } while(u8g2.nextPage());*/
+    
   delay(100);
 }
