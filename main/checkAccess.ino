e27f40b234542c8de49b8dfb1b14d080b3cb7be8
LeoAnderssonKTH <132525577+LeoAnderssonKTH@users.noreply.github.com>
2024-05-06 15:21:26 +0200

    check rfid access for meeeting

---
+++ main/checkAccess.ino
@@ -0,0 +1,12 @@
+bool checkAccess(){
+  String pathToRFID = "test/" + uid + "/" + currentDate + "/" + currentMeetingID + "/rfid";
+
+    if(Firebase.Firestore.getDocument(&fbdo, PROJECT_ID, "", pathToRFID.c_str(), "")) {
+
+      return true;
+   
+  } else {
+    Serial.println(fbdo.errorReason());
+  }
+    return false;
+}
